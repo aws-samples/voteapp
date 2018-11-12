d6d59904be3bfcde527a0f965aa1a5d45ab021da
Tony Pujals <tony@subfuzion.com>
2018-11-11 18:36:18 -0800

    refactor mesh deploy script

--- aws/servicemesh/deploy.sh
+++ aws/servicemesh/deploy.sh
@@ -1,75 +1,103 @@
-#!/bin/bash
-set -e
+#!/bin/sh
 
-#TODO: remove this when mesh an be replaced with full cli command
+# TODO: remove this when mesh an be replaced with full cli command
 if [ -f meshvars.sh ]; then
     source meshvars.sh
 fi
 
+# Ensure your AWS profile (specified in meshvars.sh) sets the default region
+MESHCMD="aws --endpoint-url $LATTICE_FRONTEND lattice'"
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-MESH_ARN=""
-
-#TODO: Replace 'mesh' with actual CLI commands
-
-
-echo ":: Creating Service Mesh ::"
-{
-    CMD='mesh create-mesh --cli-input-json file://config/create-mesh-voteapp.json'
-    echo "Running:: $CMD"
-    OUTPUT=`$CMD`
-    MESH_ARN=$(echo $OUTPUT | jq -r '.mesh.metadata.arn')
-    echo "MESH_ARN:: $MESH_ARN"
-} || {
-    echo "Err: Unable to create service mesh: $?"
-    exit 1
-
+print() {
+    printf "[MESH] %s\n" "$*"
 }
 
+err() {
+    msg="Error: $*"
+    print $msg
+    code=${2:-"1"}
+    exit $code
+}
 
-
-echo ""
-echo ":: Creating Virtual Nodes ::"
-
-for service in $(ls $DIR/config/virtualnodes); do
+create_mesh() {
+    print "Creating service mesh"
     {
-        CMD="mesh create-virtual-node --cli-input-json file://config/virtualnodes/$service"
-        echo "Running:: $CMD"
-        OUTPUT=`$CMD`
-        echo $OUTPUT
+        CMD="$MESHCMD create-mesh --cli-input-json file://config/create-mesh-voteapp.json"
+        print "run: $CMD"
+        OUTPUT=$($CMD)
+        MESH_ARN=$(echo $OUTPUT | jq -r '.mesh.metadata.arn')
+        print "MESH_ARN: $MESH_ARN"
     } || {
-        echo "Err: Unable to create virtual node: $?"
-        exit 1
+        err "Unable to create service mesh" "$?"
     }
-done
+}
 
-echo ""
-echo ":: Creating Virtual Routers ::"
-
-for service in $(ls $DIR/config/virtualrouters); do
+create_virtual_node() {
+    service=$1
     {
-        CMD="mesh create-virtual-router --cli-input-json file://config/virtualrouters/$service"
-        echo "Running:: $CMD"
-        OUTPUT=`$CMD`
-        echo $OUTPUT
+        CMD="$MESHCMD create-virtual-node --cli-input-json file://config/virtualnodes/$service"
+        print "run: $CMD"
+        OUTPUT=$($CMD)
+        print $OUTPUT
     } || {
-        echo "Err: Unable to create virtual router: $?"
-        exit 1
+        err "Unable to create virtual node" "$?"
     }
-done
+}
 
+create_virtual_nodes() {
+    print "Creating virtual nodes"
+    print "======================"
+    for service in $(ls $DIR/config/virtualnodes); do
+        create_virtual_node $service
+    done
+}
 
-echo ""
-echo ":: Creating Virtual Routes ::"
-
-for service in $(ls $DIR/config/virtualrouters); do
+create_virtual_router() {
+    service=$1
     {
-        CMD="mesh create-route --cli-input-json file://config/virtualroutes/$service"
-        echo "Running:: $CMD"
-        OUTPUT=`$CMD`
-        echo $OUTPUT
+        CMD="$MESHCMD create-virtual-router --cli-input-json file://config/virtualrouters/$service"
+        print "run: $CMD"
+        OUTPUT=$($CMD)
+        print $OUTPUT
     } || {
-        echo "Err: Unable to create virtual router: $?"
-        exit 1
+        err "Unable to create virtual router" "$?"
     }
-done
+}
+
+create_virtual_routers() {
+    print "Creating virtual routers"
+    print "========================"
+    for service in $(ls $DIR/config/virtualrouters); do
+        create_virtual_router $service
+    done
+}
+
+create_virtual_route() {
+    service=$1
+    {
+        CMD="$MESHCMD create-route --cli-input-json file://config/virtualroutes/$service"
+        print "run: $CMD"
+        OUTPUT=$($CMD)
+        print $OUTPUT
+    } || {
+        err "Unable to create virtual route" "$?"
+    }
+}
+
+create_virtual_routes() {
+    print "Creating virtual routes"
+    print "======================="
+    for service in $(ls $DIR/config/virtualrouters); do
+        create_virtual_route $service
+    done
+}
+
+main() {
+    create_mesh
+    create_virtual_nodes
+    create_virtual_routers
+}
+
+main
