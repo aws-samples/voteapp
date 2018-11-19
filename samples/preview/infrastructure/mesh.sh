146500d1b537c5e4178fdb1e8afc70a96289ba49
Tony Pujals <tony@subfuzion.com>
2018-11-19 13:36:04 -0800

    mesh.sh needs --endpoint-url option

--- samples/preview/infrastructure/mesh.sh
+++ samples/preview/infrastructure/mesh.sh
@@ -4,6 +4,6 @@ set -ex
 
 action=${1:-"update-mesh"}
 
-aws --profile ${AWS_PROFILE} --region ${AWS_REGION} \
+aws --profile ${AWS_PROFILE} --region ${AWS_REGION} --endpoint-url $LATTICE_FRONTEND \
     lattice ${action} \
     --mesh-name ${MESH_NAME}
