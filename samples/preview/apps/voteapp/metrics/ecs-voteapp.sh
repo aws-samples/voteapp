40f6505ef0659345907e99d3e14eede71fd08bf7
Grandhi <gmridula@88e9fe53311c.ant.amazon.com>
2018-11-23 17:16:04 -0500

    Prometheus-Grafana Integration

---
+++ samples/preview/apps/voteapp/metrics/ecs-voteapp.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -ex
+
+ACTION=${1:-"update-stack"}
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+err() {
+    msg="Error: $1"
+    print ${msg}
+    code=${2:-"1"}
+    exit ${code}
+}
+
+describe_virtual_node() {
+    service=$1
+    cmd=( aws --profile ${AWS_PROFILE} --region ${AWS_REGION} --endpoint-url ${LATTICE_FRONTEND} \
+                lattice describe-virtual-node  \
+                --mesh-name ${MESH_NAME} --virtual-node-name ${service} \
+                --query virtualNode.metadata.uid --output text )
+    node_id=$("${cmd[@]}") || err "Unable to describe node ${service}" "$?"
+    echo ${node_id}
+}
+
+VOTE_WEB_NODE_ID=$(describe_virtual_node "web-vn")
+VOTE_REPORTS_NODE_ID=$(describe_virtual_node "reports-vn")
+
+aws --profile ${AWS_PROFILE} --region ${AWS_REGION} \
+    cloudformation ${ACTION} \
+    --stack-name ${ENVIRONMENT_NAME}-ecs-voteapp \
+    --capabilities CAPABILITY_IAM \
+    --template-body file://${DIR}/ecs-voteapp.yaml  \
+    --parameters \
+    ParameterKey=EnvironmentName,ParameterValue=${ENVIRONMENT_NAME} \
+    ParameterKey=VoteWebNodeId,ParameterValue="${VOTE_WEB_NODE_ID}" \
+    ParameterKey=VoteReportsNodeId,ParameterValue="${VOTE_REPORTS_NODE_ID}"
