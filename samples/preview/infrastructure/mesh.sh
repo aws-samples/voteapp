b3598bc6737bae353d7348d971f4a7cef7dc4b60
Tony Pujals <tony@subfuzion.com>
2018-11-27 12:21:32 -0800

    wip

--- samples/preview/infrastructure/mesh.sh
+++ samples/preview/infrastructure/mesh.sh
@@ -1,9 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -ex
 
-action=${1:-"update-mesh"}
+actions=("$@")
 
-aws --profile ${AWS_PROFILE} --region ${AWS_REGION} --endpoint-url $APPMESH_FRONTEND \
-    appmesh ${action} \
+if [ "${#actions[@]}" -eq 0 ]; then
+    actions+=("create-mesh")
+fi
+
+aws --profile ${AWS_PROFILE} \
+    --region ${AWS_REGION} \
+    appmesh \
+    "${actions[@]}" \
     --mesh-name ${MESH_NAME}
