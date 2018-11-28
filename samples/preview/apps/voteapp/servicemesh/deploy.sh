fbc82c2db8de1f7b08c209581f02683e4948ee1f
Tony Pujals <tony@subfuzion.com>
2018-11-28 04:45:59 -0800

    update deploy and delete mesh scripts

--- samples/preview/apps/voteapp/servicemesh/deploy.sh
+++ samples/preview/apps/voteapp/servicemesh/deploy.sh
@@ -33,8 +33,8 @@ sanity_check() {
 }
 
 create_mesh() {
-    print "Creating service mesh"
-    print "====================="
+    print "Creating/updating service mesh"
+    print "=============================="
     cmd=( aws --region ${AWS_REGION} $MESHCMD create-mesh --mesh-name ${MESH_NAME} --query mesh.metadata.arn --output text )
     print "${cmd[@]}"
     arn=$("${cmd[@]}") || err "Unable to create service mesh" "$?"
@@ -45,7 +45,7 @@ create_virtual_node() {
     service=$1
     cmd=( aws --region ${AWS_REGION} $MESHCMD create-virtual-node --mesh-name ${MESH_NAME} --cli-input-json file:///${DIR}/config/virtualnodes/${service} --query virtualNode.metadata.uid --output text )
     print "${cmd[@]}"
-    uid=$("${cmd[@]}") || update_virtual_node ${service}
+    uid=$("${cmd[@]}") || err "Unable to create/update virtual node" "$?"
     print "--> ${uid}"
 }
 
@@ -53,15 +53,15 @@ update_virtual_node() {
     service=$1
     cmd=( aws --region ${AWS_REGION} $MESHCMD update-virtual-node --mesh-name ${MESH_NAME} --cli-input-json file:///${DIR}/config/virtualnodes/${service} --query virtualNode.metadata.uid --output text )
     print "${cmd[@]}"
-    uid=$("${cmd[@]}") || err "Unable to create/update virtual node" "$?"
+    uid=$("${cmd[@]}" 2>/dev/null) || create_virtual_node ${service}
     print "--> ${uid}"
 }
 
-create_virtual_nodes() {
-    print "Creating virtual nodes"
-    print "======================"
+configure_virtual_nodes() {
+    print "Creating/updating virtual nodes"
+    print "==============================="
     for service in $(ls ${DIR}/config/virtualnodes); do
-        create_virtual_node ${service}
+        update_virtual_node ${service}
     done
 }
 
@@ -69,7 +69,7 @@ create_virtual_router() {
     service=$1
     cmd=( aws --region ${AWS_REGION} $MESHCMD create-virtual-router --mesh-name ${MESH_NAME} --cli-input-json file:///${DIR}/config/virtualrouters/${service} --query virtualRouter.metadata.uid --output text )
     print "${cmd[@]}"
-    uid=$("${cmd[@]}") || update_virtual_router ${service}
+    uid=$("${cmd[@]}") || err "Unable to create/update virtual router" "$?"
     print "--> ${uid}"
 }
 
@@ -77,15 +77,15 @@ update_virtual_router() {
     service=$1
     cmd=( aws --region ${AWS_REGION} $MESHCMD update-virtual-router --mesh-name ${MESH_NAME} --cli-input-json file:///${DIR}/config/virtualrouters/${service} --query virtualRouter.metadata.uid --output text )
     print "${cmd[@]}"
-    uid=$("${cmd[@]}") || err "Unable to create/update virtual router" "$?"
+    uid=$("${cmd[@]}" 2>/dev/null) || create_virtual_router ${service}
     print "--> ${uid}"
 }
 
-create_virtual_routers() {
-    print "Creating virtual routers"
-    print "========================"
+configure_virtual_routers() {
+    print "Creating/updating virtual routers"
+    print "================================="
     for service in $(ls ${DIR}/config/virtualrouters); do
-        create_virtual_router ${service}
+        update_virtual_router ${service}
     done
 }
 
@@ -93,7 +93,7 @@ create_route() {
     service=$1
     cmd=( aws --region ${AWS_REGION} $MESHCMD create-route --mesh-name ${MESH_NAME} --cli-input-json file:///${DIR}/config/routes/${service} --query route.metadata.uid --output text )
     print "${cmd[@]}"
-    uid=$("${cmd[@]}") || update_route ${service}
+    uid=$("${cmd[@]}") || err "Unable to create/update route" "$?"
     print "--> ${uid}"
 }
 
@@ -101,24 +101,24 @@ update_route() {
     service=$1
     cmd=( aws --region ${AWS_REGION} $MESHCMD update-route --mesh-name ${MESH_NAME} --cli-input-json file:///${DIR}/config/routes/${service} --query route.metadata.uid --output text )
     print "${cmd[@]}"
-    uid=$("${cmd[@]}") || err "Unable to create/update route" "$?"
+    uid=$("${cmd[@]}" 2>/dev/null) || create_route ${service}
     print "--> ${uid}"
 }
 
-create_routes() {
-    print "Creating routes"
-    print "======================="
+configure_routes() {
+    print "Creating/updating routes"
+    print "========================"
     for service in $(ls ${DIR}/config/routes); do
-        create_route ${service}
+        update_route ${service}
     done
 }
 
 main() {
     sanity_check
     create_mesh
-    create_virtual_nodes
-    create_virtual_routers
-    create_routes
+    configure_virtual_nodes
+    configure_virtual_routers
+    configure_routes
 }
 
 main
