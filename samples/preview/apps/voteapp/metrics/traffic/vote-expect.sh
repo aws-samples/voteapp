e88ccc81091b90ab6672484af9d788ec1cd4ad8b
EC2 Default User <ec2-user@ip-172-31-2-64.us-west-2.compute.internal>
2018-11-28 16:36:29 +0000

    Added pattern for vote selection. Will be dogs-cats-dogs (uparrow-downarrow-uparrow)

--- samples/preview/apps/voteapp/metrics/traffic/vote-expect.sh
+++ samples/preview/apps/voteapp/metrics/traffic/vote-expect.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/expect -f
 spawn ./vote.sh
 expect "*What do you like better*"
+#Represents up arrow selection. So will select the last option - Dogs
+send "\033\[A"
+send -- "\r"
+interact
+spawn ./vote.sh
+expect "*What do you like better*"
+#Represents down arrow selection. So will select the last option - Cats
+send "\033\[B"
+send -- "\r"
+interact
+spawn ./vote.sh
+expect "*What do you like better*"
+#Represents up arrow selection. So will select the last option - Dogs
 send "\033\[A"
 send -- "\r"
 interact
